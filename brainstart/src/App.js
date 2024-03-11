f6fcd694e884c922c25f5597d7253c31b3dbd420
unknown <LICEU JN@DESKTOP-4751FNP.liceu.local>
2024-03-11 14:47:47 -0300

    terceiro commit

--- brainstart/src/App.js
+++ brainstart/src/App.js
@@ -6,23 +6,42 @@ function App() {
   return (
     <div className="App">
       <body>
-        <header>
-          <div className='top'>
+         <header>
+           <div className='top'>
             <h2> CardápioFácil </h2>
-          </div>
-            <div className='opcoes'>
-              <ul>
-                <li><a href='#' className='A'> Cardápio </a></li>
-                <li><a href='#' className='B'> Ordem da Fila  </a></li>
-              </ul>
-            </div>
-          <div className='imagem'>
-            <a href='#'>
-              <img src={im}></img>
-            </a>
-
+           </div>
+             <div className='opcoes'>
+               <ul>
+                 <li><a href='#' className='A'> Cardápio </a></li>
+                 <li><a href='#' className='B'> Ordem da Fila  </a></li>
+               </ul>
+             </div>
+           <div className='imagem'>
+             <a href='#'>
+               <img src={im}></img>
+             </a>
           </div>
         </header>
+        <main>
+          <div className='container'>
+            <div className='bloco1'>
+              <h3>seg</h3>
+            </div>
+            <div className='bloco1'>
+              <h3>ter</h3>
+            </div>
+            <div className='bloco1'>
+              <h3>qua</h3>
+            </div>
+            <div className='bloco1'>
+              <h3>qui</h3>
+            </div>
+            <div className='bloco1'>
+              <h3>sex</h3>
+            </div>
+          </div>  
+         </main>
+
       </body>
     </div>
   );
